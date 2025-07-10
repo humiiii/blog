a231e800698d222dbdbbaa5508edc86064b920cf
Muhammad Umaid Rather <107248121+humiiii@users.noreply.github.com>
2025-07-10 21:00:27 +0530

    initial commit and structure

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./components/Navbar.component";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Navbar.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <p className="font-medium text-2xl font-gelasio">Diary</p>
+    </nav>
+  );
+};
+
+export default Navbar;
